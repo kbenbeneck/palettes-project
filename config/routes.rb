7202132c6f1c785289e0ea025d798aa9f2a2c6f9
KBENBENECK <kbenbeneck@gmail.com>
2020-10-26 02:31:56 -0400

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :tones
-  resources :palettes
+  resources :tones, only: [:index]
+  resources :palettes, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
