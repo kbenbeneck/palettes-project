070d4548042bcc1954375fcf1e185be5e3b6f633
KBENBENECK <kbenbeneck@gmail.com>
2020-10-26 02:31:05 -0400

    add palette index and show controller actions

--- app/controllers/palettes_controller.rb
+++ app/controllers/palettes_controller.rb
@@ -1,2 +1,19 @@
 class PalettesController < ApplicationController
+    def index
+        palettes = Palette.all
+        render json: palettes, include: [:tones] 
+    end
+
+    def show
+        palatte = Palette.find_by(id: params[:id])
+        if palatte
+            render json: {
+                id: palatte.id, 
+                name: palatte.name, 
+                background: palatte.background, 
+                tones: palatte.tones }
+        else
+            render json: { message: 'Nope' }
+        end
+    end
 end
