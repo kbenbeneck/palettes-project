da623c1a322a65e26911e26733c558c313898c24
KBENBENECK <kbenbeneck@gmail.com>
2020-11-01 11:40:14 -0500

    Enter es6 class
    Index link now uses class prototype method to render explicit html, instead of appending and setting attr for multiple elements.
    Form now reflects the resource attr changes(-name)
    Begin post/create methods

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,138 +2,132 @@ const BASE_URL = "http://localhost:3000"
 const PALETTES_URL = `${BASE_URL}/palettes`
 const TONES_URL = `${BASE_URL}/tones`
 const main = document.querySelector('main')
-
-
+//Home****************************************************************************
 document.addEventListener('DOMContentLoaded', () => {
     displayGif()
 })
-
 function displayGif() {
     clearMain()
     main.innerHTML = `<img src="./media/elbow.gif" alt="elbow" width="100%" id="gif"/>`
 }
-
+//PalettesIndex******************************************************************
 function getPalettes() {
     clearMain()
     fetch(PALETTES_URL)
     .then(resp => resp.json())
-    .then(ps => {
-        ps.forEach(p =>{
-            renderPalettes(p)
+    .then(palettes => {
+        palettes.forEach(palette =>{
+            let pal = new Pal(palette)
+            main.innerHTML += pal.renderPalette()
         })
     })
 }
 
-function getTones() {
-    clearMain()
-    fetch(TONES_URL)
-    .then(resp => resp.json())
-    .then(tones => renderTones(tones))
-    
-}
-
-function renderPalettes(p) {
-    let pDiv = document.createElement('div')
-    pDiv.setAttribute('id', `${p.id}`)
-    let pName = document.createElement('p')
-    pName.innerText = p.name
-    let pBackground = document.createElement('p')
-    pBackground = p.background 
-    let pTones = document.createElement('ul')
-    pTones.setAttribute('id', `${p.id}`)
-    p.tones.forEach(tone => {
-        let toneLI = document.createElement('li')
-        toneLI.innerText = `${tone.hex}`
-        toneLI.setAttribute('id', `${tone.id}`)
-        pTones.appendChild(toneLI)
-    })
-    pDiv.append(pName, pBackground, pTones)
-    main.appendChild(pDiv)
-}
-function renderTones(tones) {
-    let tUL = document.createElement('ul')
-    tones.forEach(tone => {
-    let tLI = document.createElement('li')
-    tLI.setAttribute('id', `${tone.id}`)
-    let tHex = document.createElement('p')
-    tHex.innerText = `${tone.hex}`
-    let pId = document.createElement('p')
-    pId.innerText = `${tone.palette_id}`
-    tUL.appendChild(tLI)
-    tLI.append(tHex, pId)
-    })
-    main.appendChild(tUL)   
-}
-
+//Form***************************************************************************
 function displayForm() {
     clearMain()
-    let formDiv = document.createElement('div')
-    formDiv.setAttribute('id', "form")
-    let html = `
-            <label>Palette Name:</label>
-            <input type="text" id="name" placeholder="or don't, just pick a color">
-            <br>
-            <label>Background</label>
-            <input type="text" id="background" placeholder="Pick a color =>">
-            <input type="color" id="palcolor">
-            <hr>
-            <div id="newPal" class="square">
-            </div>
-            <label>Pick some color tones</label>
-            <input type="color" id="tonecolor">
-            <ul id="tonesList">
-            </ul>
-            <button type="button" id="reset">Reset</button>
-            <button type="button" id="save">Save</button>
-            ` 
-    formDiv.innerHTML = html
-    main.appendChild(formDiv) 
+    addFormHtml()
     let colorInput = document.querySelector("#palcolor")
     let bgInput = document.querySelector("input#background")
-    let newPal = document.querySelector("div#newPal")
+    let newPal = document.querySelector(".square")
     colorInput.addEventListener('input', () => {
         let color = colorInput.value
         bgInput.value = color
         newPal.style.backgroundColor = color
+        
     })
-    let toneInput = document.querySelector("#tonecolor")
-    let counter = 0; counter < 9;
-    let ul = document.getElementById('tonesList')
-    toneInput.addEventListener('input', () => {
-        let toneColor = toneInput.value
-        if (ul.childElementCount < 9 && ul.lastElementChild != toneColor) {
-        addTones(toneColor)
+    
+    function addFormHtml() {
+        let formDiv = document.createElement('div')
+        formDiv.setAttribute('id', "form")
+        let html = `
+            <br>
+            <form id="pform">
+                <label>Palette Background</label>
+                <input type="text" id="background" placeholder="Pick a color =>">
+                <input type="color" id="palcolor">
+                <div class="square">
+                </div>
+            </form>    
+            <hr>
+            <label>Pick 9 tones</label>
+            <input type="color" id="tonecolor">
+            <ul id="tonesList">
+            </ul>
+            <button type="button" id="reset">Reset</button>
+            <button type="button" id="save">Save</button>        
+            ` 
+        formDiv.innerHTML = html
+        main.appendChild(formDiv)
+
+        
+        let toneInput = document.querySelector("#tonecolor")
+        let counter = 0; counter < 9;
+        let ul = document.getElementById('tonesList')
+
+        toneInput.addEventListener('input', () => {
+            let toneColor = toneInput.value
+            if (ul.childElementCount < 9 && ul.lastElementChild !== toneColor) {
+            addTones(toneColor)
+            }
+        })
+
+        function addTones(toneColor) {   
+            let li = document.createElement('li')
+            li.setAttribute('id', "tone" + counter++)
+            li.setAttribute('hex', `${toneColor}`)
+            li.innerText = `${toneColor}`
+            li.setAttribute('class', "toneLI")
+            ul.appendChild(li)
+            document.querySelector('li:last-child').style.backgroundColor = toneColor    
         }
 
-    })
-    function addTones(toneColor) {
-        let li = document.createElement('li')
-        li.setAttribute('id', "tone" + counter++)
-        li.setAttribute('hex', `${toneColor}`)
-        li.innerText = `${toneColor}`
-        li.setAttribute('class', "toneLI")
-        ul.appendChild(li)
-        document.querySelector('li:last-child').style.backgroundColor = toneColor
-    }
-    document.getElementById("reset").addEventListener('click', clearPalette)
-    
+        document.getElementById("reset").addEventListener('click', clearPalette)
+        document.getElementById("save").addEventListener('click', postPaletteTones)
 
-    function clearPalette() {
-        counter = 0
-        ul.innerHTML = ""
-    }
-    
+        function clearPalette() {
+            counter = 0
+            ul.innerHTML = ""
+        }
+    }   
 }
 
 
 
 
+function postPaletteTones() {
+    
+}
+
+//Helpers**********************************************************************
 function clearMain() {
     main.innerHTML = ""
 }
 
-
 document.getElementById("palettes").addEventListener('click', getPalettes)
-document.getElementById("tones").addEventListener('click', getTones)
 document.getElementById("home").addEventListener('click', displayGif)
 document.getElementById("paletteForm").addEventListener('click', displayForm)
+
+//Classes**********************************************************************
+
+class Pal {
+    constructor(palette) {
+        this.id = palette.id
+        this.background = palette.background
+        this.tones = palette.tones
+    }
+    
+    renderPalette() {
+        return `
+        <div id="palette-${this.id}" class="square>
+            <a href="#" data-id="${this.id}">${this.background}</a>
+            <ul id="tones"></ul>
+            <button id="delete" data-id=${this.id}">Delete</button>
+            <button id="update-palette" data-id=${this.id}">Edit</button>
+            <button id="add-tones" data-id=${this.id}">Add Tones</button>        
+        </div>
+        `
+    }
+  
+}
+
