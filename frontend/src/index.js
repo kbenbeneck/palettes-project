4c34c2ccee50176af518bdc4bb70890cc53b8ed4
KBENBENECK <kbenbeneck@gmail.com>
2020-11-06 01:17:52 -0500

    added link to and palette show page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,7 +23,7 @@ function displayGif() {
     function about() {
         return `
         <div id="aboutDiv">
-           
+            
             <p id-"aboutText">
             I needed to bring together my passion for creating geometric opart with my desire to earn income as a developer one day. 
               
@@ -49,6 +49,7 @@ function getPalettes() {
             
             pal.renderIndexTones()
         })
+        attachClickToLinks()
     })
 }
 
@@ -159,6 +160,44 @@ function createPalette() {
     
     
 }
+//Show Page************************************************************************
+function attachClickToLinks() {
+    let palettes = document.querySelectorAll('#index a')
+    palettes.forEach(palette => {
+        palette.addEventListener('click', showPalette)
+    })
+}
+
+function showPalette() {
+    clearMain()
+    let id = event.target.dataset.id
+        
+    fetch(PALETTES_URL+`/${id}`)
+    .then(resp => resp.json())
+    .then(palette => {
+        let p = new Pal(palette)
+        main.innerHTML += p.renderPalette()
+        let pdiv = document.querySelector(`div#palette-${p.id}`)  
+        pdiv.style.background = `${p.background}`
+        let ul = document.querySelector(`div #tones`)
+        let counter = 0; counter < 9;
+        p.tones.forEach(tone => {
+            let li = document.createElement('li')
+            li.setAttribute('id', "t" + counter++)
+            li.setAttribute('class', "toneLI")
+            li.setAttribute('hex', `${tone.hex}`)
+            li.innerText = `${tone.hex}`
+            ul.appendChild(li)  
+            let fTones = document.querySelectorAll('li'); 
+            for (let li of fTones){
+                li.style.background = li.innerText 
+            }
+        })  
+    })
+ 
+    
+    
+}
 
 //MainHelpers**********************************************************************
 
@@ -196,6 +235,8 @@ class Pal {
         
          
         `
+        
+        
     }
 
     renderTones() {
